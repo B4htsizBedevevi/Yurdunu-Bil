5b38ab440913ce5ce9065d3830b480a7d26bf3d0
BahtsizBedevi <55953865+B4htsizBedevevi@users.noreply.github.com>
2026-09-04 22:23:23 +0300

    chore: align app version 27.0.1

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.YURDUNUBIL_CONFIG={
   SUPABASE_URL:"https://rdgefzwvfqvzmpfoiprj.supabase.co",
   SUPABASE_PUBLISHABLE_KEY:"sb_publishable_LjA0E5FY6t6ID0YMwjJpxA_vCRNCuY7",
-  APP_VERSION:"27.0.0",
+  APP_VERSION:"27.0.1",
   APP_NAME:"Yurdunu Bil",
   EXAM_DATE:"2026-10-04T10:15:00+03:00"
 };
